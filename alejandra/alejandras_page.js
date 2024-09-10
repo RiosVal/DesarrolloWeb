8023fe6e897533bc07704e2efc26a828c076e6f6
Alejnadra Diez <pelu1033@gmail.com>
2024-09-09 20:34:10 -0500

    intento subir el git 7

--- alejandra/alejandras_page.js
+++ alejandra/alejandras_page.js
@@ -10,3 +10,29 @@ document.getElementById('theme-toggle').addEventListener('click', function() {
         themeIcon.alt = 'Light Theme';
     }
 });
+
+// Obtén todas las imágenes con la clase "about-me-image"
+var images = document.getElementsByClassName('about-me-image');
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
+// Función para abrir el modal
+function openModal(img) {
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+// Función para cerrar el modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Agrega un evento de clic a cada imagen
+for (var i = 0; i < images.length; i++) {
+  images[i].onclick = function() {
+    openModal(this);
+  }
+}
